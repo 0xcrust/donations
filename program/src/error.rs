64161f0145f88ebfef76bdcd66a0b0021a7161a3
crustydev <ademola1145@gmail.com>
2022-12-27 02:15:45 +0100

    Custom error struct and messages

---
+++ program/src/error.rs
@@ -0,0 +1,21 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum CampaignError {
+    #[error("Could not deserialize instruction data")]
+    InvalidInstruction,
+    #[error("Invalid Campaign Account data")]
+    InvalidAccountData,
+    #[error("Account different from expected")]
+    AccountMismatch,
+    #[error("Account has been created already")]
+    AccountAlreadyInitialized,
+}
+
+impl From<CampaignError> for ProgramError {
+    fn from(e: CampaignError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
