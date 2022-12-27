3f174a931a9ef4da8a3ee12e8cde099dadafdfe7
crustydev <ademola1145@gmail.com>
2022-12-27 11:20:11 +0100

    Add amount_raised field to Campaign information

--- program/src/state.rs
+++ program/src/state.rs
@@ -10,9 +10,10 @@ use arrayref::{array_ref, array_refs, array_mut_ref, mut_array_refs};
 pub struct Campaign {
     pub is_initialized: bool,
     pub authority: Pubkey,
+    pub vault: Pubkey,
     pub description: [u8; 200],
     pub target: u64,
-    pub balance: u64,
+    pub amount_raised: u64,
     pub bump: u8,
 }
 
@@ -25,30 +26,31 @@ impl IsInitialized for Campaign {
 }
 
 impl Pack for Campaign {
-    const LEN: usize = 250;
+    const LEN: usize = 282;
     fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
         let src = array_ref![src, 0, Campaign::LEN];
         let (
             is_initialized,
             authority,
+            vault,
             description,
             target,
-            balance,
+            amount_raised,
             bump,
-        ) = array_refs![src, 1, 32, 200, 8, 8, 1];
+        ) = array_refs![src, 1, 32, 32, 200, 8, 8, 1];
         let is_initialized = match is_initialized {
             [0] => false,
             [1] => true,
             _ => return Err(ProgramError::InvalidAccountData),
         };
 
-        //let description: [u8; 200] = *<&[u8; 200] as TryInto<T>>::try_into(description)?;
         Ok(Campaign {
             is_initialized,
             authority: Pubkey::new_from_array(*authority),
+            vault: Pubkey::new_from_array(*vault),
             description: *description,
             target: u64::from_le_bytes(*target),
-            balance: u64::from_le_bytes(*balance),
+            amount_raised: u64::from_le_bytes(*amount_raised),
             bump: bump[0],
         })
     }
@@ -58,17 +60,19 @@ impl Pack for Campaign {
         let (
             is_initialized_dst,
             authority_dst,
+            vault_dst,
             description_dst,
             target_dst,
-            balance_dst,
+            amount_raised_dst,
             bump_dst
-        ) = mut_array_refs![dst, 1, 32, 200, 8, 8, 1];
+        ) = mut_array_refs![dst, 1, 32, 32, 200, 8, 8, 1];
 
         is_initialized_dst[0] = self.is_initialized as u8;
         authority_dst.copy_from_slice(self.authority.as_ref());
+        vault_dst.copy_from_slice(self.vault.as_ref());
         description_dst.copy_from_slice(self.description.as_ref());
         *target_dst = self.target.to_le_bytes();
-        *balance_dst = self.balance.to_le_bytes();
+        *amount_raised_dst = self.amount_raised.to_le_bytes();
         bump_dst[0] = self.bump;
     }
 }
