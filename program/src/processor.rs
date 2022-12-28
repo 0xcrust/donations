922a42b0ec6e2e4a26a076aef8c7e6083917490a
crustydev <ademola1145@gmail.com>
2022-12-28 20:34:15 +0100

    Fix logic errors found during testing

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -2,7 +2,7 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
-    program::invoke_signed,
+    program::{invoke, invoke_signed},
     program_error::ProgramError,
     program_pack::Pack,
     pubkey::Pubkey,
@@ -36,7 +36,7 @@ impl Processor {
                 Self::process_donate(accounts, program_id, amount)
             },
             Instruction::Withdraw {} => {
-                msg!("Withdrawing...");
+                msg!("Instruction: Withdraw");
                 Self::process_withdraw(accounts, program_id)
             }
         }
@@ -50,18 +50,25 @@ impl Processor {
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
         let fundstarter = next_account_info(account_info_iter)?;
+    
+        msg!("Verify signer");
         if !fundstarter.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
-        let (campaign_pda, campaign_bump) = Pubkey::find_program_address(&[b"state".as_ref(), fundstarter.key.as_ref()], program_id);
+        msg!("Verify campaign account seeds");
+        let (campaign_pda, campaign_bump) = Pubkey::find_program_address(&[b"campaign".as_ref(), fundstarter.key.as_ref()], program_id);
         let campaign_state = next_account_info(account_info_iter)?;
         if &campaign_pda != campaign_state.key {
             return Err(CampaignError::AccountMismatch.into());
         }
-        if !(**campaign_state.try_borrow_lamports()? > 0) {
+        msg!("Check if campaign account is initialized");
+        if **campaign_state.try_borrow_lamports()? > 0 {
             return Err(CampaignError::AccountAlreadyInitialized.into());
         }
+
+        let vault_account = next_account_info(account_info_iter)?;
+        let system_program = next_account_info(account_info_iter)?;
 
         let lamports = Rent::default().minimum_balance(Campaign::LEN);
         let create_campaign_state_ix = solana_program::system_instruction::create_account(
@@ -69,22 +76,25 @@ impl Processor {
             campaign_state.key,
             lamports,
             Campaign::LEN as u64,
-            fundstarter.key,
+            program_id,
         );
         let state_seeds = &[
-            b"state".as_ref(),
+            b"campaign".as_ref(),
             fundstarter.key.as_ref(),
             &[campaign_bump]
         ];
+        msg!("Create campaign instruction");
         invoke_signed(
             &create_campaign_state_ix,
             &[
                 fundstarter.clone(),
-                campaign_state.clone(),  
+                campaign_state.clone(),
+                system_program.clone(),  
             ],
             &[&state_seeds[..]],
         )?;
 
+        msg!("Write to campaign account");
         let mut campaign_info = Campaign::unpack_unchecked(&campaign_state.try_borrow_data()?)?;
         campaign_info.is_initialized = true;
         campaign_info.authority = *fundstarter.key;
@@ -93,12 +103,12 @@ impl Processor {
         campaign_info.amount_raised = 0;
         campaign_info.bump = campaign_bump;
         
-        let (vault_pda, vault_bump) = Pubkey::find_program_address(&[b"vault".as_ref(), fundstarter.key.as_ref()], program_id);
-        let vault_account = next_account_info(account_info_iter)?;
+        msg!("Verify vault account seeds");
+        let (vault_pda, vault_bump) = Pubkey::find_program_address(&[b"vault".as_ref(), campaign_state.key.as_ref()], program_id);
         if &vault_pda != vault_account.key {
             return Err(CampaignError::AccountMismatch.into());
         }
-        if !(**vault_account.try_borrow_lamports()? > 0) {
+        if **vault_account.try_borrow_lamports()? > 0 {
             return Err(CampaignError::AccountAlreadyInitialized.into());
         }
         let lamports = Rent::default().minimum_balance(0);
@@ -107,23 +117,27 @@ impl Processor {
             vault_account.key,
             lamports,
             0,
-            fundstarter.key,
+            program_id,
         );
         let vault_seeds = &[
             b"vault".as_ref(),
-            fundstarter.key.as_ref(),
+            campaign_state.key.as_ref(),
             &[vault_bump]
         ];
+
+        msg!("Create vault instruction");
         invoke_signed(
             &create_vault_ix,
             &[
                 fundstarter.clone(),
-                vault_account.clone(),  
+                vault_account.clone(),
+                system_program.clone(),  
             ],
             &[&vault_seeds[..]],
         )?;
 
         campaign_info.vault = *vault_account.key;
+        Campaign::pack(campaign_info, &mut campaign_state.try_borrow_mut_data()?)?;
         Ok(())
     }
 
@@ -134,15 +148,18 @@ impl Processor {
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
         let donator = next_account_info(account_info_iter)?;
+       
+        msg!("Verify signer");
         if !donator.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
         let campaign_state = next_account_info(account_info_iter)?;
         if campaign_state.owner != program_id {
-            msg!("Account is not owned by the account(not a PDA)");
             return Err(ProgramError::IncorrectProgramId);
         }
+        
+        msg!("Unpack campaign account info");
         let mut campaign_info = Campaign::unpack(&campaign_state.try_borrow_data()?)?;
 
         let vault = next_account_info(account_info_iter)?;
@@ -150,17 +167,17 @@ impl Processor {
             return Err(CampaignError::AccountMismatch.into());
         }
 
-        **donator.try_borrow_mut_lamports()? = donator
-            .lamports()
-            .checked_sub(amount)
-            .unwrap();
-
-        **vault.try_borrow_mut_lamports()? = vault
-            .lamports()
-            .checked_add(amount)
-            .unwrap();
+        msg!("Transfer from donator to vault");
+        let system_program = next_account_info(account_info_iter)?;
+        let donate_ix = solana_program::system_instruction::transfer(
+            donator.key,
+            vault.key,
+            amount,
+        );
+        invoke(&donate_ix, &[donator.clone(), vault.clone(), system_program.clone()])?;
 
         campaign_info.amount_raised = campaign_info.amount_raised.checked_add(amount).unwrap();
+        Campaign::pack(campaign_info, &mut campaign_state.try_borrow_mut_data()?)?;
         Ok(())
     }
 
@@ -170,26 +187,32 @@ impl Processor {
     ) -> ProgramResult {
         let account_info_iter = &mut accounts.iter();
         let fundstarter = next_account_info(account_info_iter)?;
+
+        msg!("Verify signer");
         if !fundstarter.is_signer {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
         let campaign_state = next_account_info(account_info_iter)?;
-        let (pda, bump) = Pubkey::find_program_address(&[b"state".as_ref(), fundstarter.key.as_ref()], program_id);
+        msg!("Verify campaign_state's seeds");
+        let (pda, bump) = Pubkey::find_program_address(&[b"campaign".as_ref(), fundstarter.key.as_ref()], program_id);
         if *campaign_state.key != pda {
             return Err(CampaignError::AccountMismatch.into());
         }
 
         let campaign_info = Campaign::unpack(&campaign_state.try_borrow_data()?)?;
+        msg!("Is fundstarter the correct authority");
         if campaign_info.authority != *fundstarter.key {
             return Err(CampaignError::WrongAuthority.into());
         }
+        msg!("Verifying bump");
         if campaign_info.bump != bump {
             return Err(CampaignError::AccountMismatch.into());
         }
 
         let vault = next_account_info(account_info_iter)?;
 
+        msg!("Close vault and credit fundstarter");
         // Withdraw all lamports to the fundstarter and close vault account
         **fundstarter.try_borrow_mut_lamports()? = fundstarter
             .lamports()
@@ -197,6 +220,7 @@ impl Processor {
             .unwrap();
         **vault.try_borrow_mut_lamports()? = 0;
 
+        msg!("Close campaign account and credit fundstarter");
         // Close state account
         **fundstarter.try_borrow_mut_lamports()? = fundstarter
             .lamports()
@@ -204,11 +228,11 @@ impl Processor {
             .unwrap();
         **campaign_state.try_borrow_mut_lamports()? = 0;
 
+        msg!("Zero account data");
         let mut data = campaign_state.try_borrow_mut_data()?;
         for byte in data.deref_mut().iter_mut() {
             *byte = 0;
         }
-
         Ok(())
     }
 }
